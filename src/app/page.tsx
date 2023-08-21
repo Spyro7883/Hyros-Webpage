3acae472e9da19916781510dfffbb8e63bf87155
Spyro7883 <spinu11_2003@yahoo.ro>
2023-08-21 22:25:18 +0300

    small changes to the background added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -397,6 +397,35 @@ export default function Home() {
             ))}
           </div>
         </section>
+        <section
+          className={`${styles.second_business_model} center-Oy d-flex clmn-dir`}
+          aria-label="Ad Spend Counter"
+        >
+          <p className={styles.features_subtitle}>
+            Click To See Features That Apply To YOUR Business Model
+          </p>
+          {boxTitle.map((box_option) => (
+            <a className={styles.menu_box} key={box_option}>
+              <div className="text-center">
+                <p className={styles.box_title}>{box_option}</p>
+                <div className={styles.circle} />
+              </div>
+              <p className={styles.box_text}>
+                The most important tools and features for Info
+              </p>
+              <div className={`${styles.circle_arrow} text-center`}>
+                <Image
+                  src="/Logo_Arrow.png"
+                  alt="Arrow Icon"
+                  className={styles.arrow_icon}
+                  width={16}
+                  height={14.5}
+                  priority
+                />
+              </div>
+            </a>
+          ))}
+        </section>
       </main>
     </>
   );
