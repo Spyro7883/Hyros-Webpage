102d05dd6586a14648b154ff3a55fc281e4f6ce2
Spyro7883 <spinu11_2003@yahoo.ro>
2023-09-09 10:46:04 +0300

    header filter & demo mobile popup added

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,30 @@
 import styles from "../styles/header.module.scss";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Header() {
+  const [popup, setPopup] = useState(false);
+  const handlePopup = () => {
+    setPopup(!popup);
+    console.log(popup);
+  };
   return (
     <>
       <header className={`${styles.header_page} d-flex center-Oy`}>
         <Image
           src="/Logo_Hyros.png"
           alt="Hyros Logo"
+          className={styles.logo}
           width={50}
           height={50}
           priority
         />
-        <div>
+        <div className={styles.trigger} onClick={handlePopup}>
           <span className={`${styles.toggle_menu} d-flex`} />
           <span className={`${styles.toggle_menu} d-flex`} />
           <span className={`${styles.toggle_menu} d-flex`} />
         </div>
+        {popup ? <div className={styles.popup} /> : ""}
       </header>
     </>
   );
