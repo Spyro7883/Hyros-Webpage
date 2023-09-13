8a736a366b8d89e957ef44dec9dc116e41454ed1
Spyro7883 <spinu11_2003@yahoo.ro>
2023-09-13 09:48:41 +0300

    responsive header completed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,37 +14,39 @@ export default function Header() {
   return (
     <>
       <header className={`${styles.header_page} d-flex center-Oy`}>
-        <Image
-          src="/Logo_Hyros.png"
-          alt="Hyros Logo"
-          className={styles.logo}
-          width={29}
-          height={29}
-          priority
-        />
-        {usersWidth >= 680 ? (
-          <nav className={`${styles.nav_bar} d-flex`}>
-            <ul className="d-flex">
-              <li className={styles.nav_link}><a>Features</a></li>
-              <li className={styles.nav_link}><a>Pricing</a></li>
-              <li className={styles.nav_link}><a>Company</a></li>
-              <li className={styles.nav_link}><a>Documentation</a></li>
-              <li className={styles.nav_link}><a>FAQ</a></li>
-              <li className={styles.nav_link}><a>Support</a></li>
-            </ul>
-            {usersWidth >= 714 ?
-              <ul className="d-flex">
-                <li className={styles.nav_link}><a>Log in</a></li>
-                <li className={styles.nav_link}><a className={styles.action}>Apply</a></li>
-              </ul> : <></>}
-          </nav>
-        ) : (
-          <div className={styles.trigger} onClick={handlePopup}>
-            <span className={`${styles.toggle_menu} d-flex`} />
-            <span className={`${styles.toggle_menu} d-flex`} />
-            <span className={`${styles.toggle_menu} d-flex`} />
-          </div>
-        )}
+        <div className={styles.head_container}>
+          <Image
+            src="/Logo_Hyros.svg"
+            alt="Hyros Logo"
+            className={styles.logo}
+            width={29}
+            height={29}
+            priority
+          />
+          {usersWidth >= 680 ? (
+            <nav className={`${styles.nav_bar} d-flex`}>
+              <ul className={`${styles.main_menu} d-flex`}>
+                <li className={styles.nav_link}><a>Features</a></li>
+                <li className={styles.nav_link}><a>Pricing</a></li>
+                <li className={styles.nav_link}><a>Company</a></li>
+                <li className={styles.nav_link}><a>Documentation</a></li>
+                <li className={styles.nav_link}><a>FAQ</a></li>
+                <li className={styles.nav_link}><a>Support</a></li>
+              </ul>
+              {usersWidth >= 722 ?
+                <ul className="d-flex">
+                  <li className={styles.nav_link}><a>Log in</a></li>
+                  <li className={`${styles.nav_link} ${styles.action}`}><a>Apply</a></li>
+                </ul> : <></>}
+            </nav>
+          ) : (
+            <div className={styles.trigger} onClick={handlePopup}>
+              <span className={`${styles.toggle_menu} d-flex`} />
+              <span className={`${styles.toggle_menu} d-flex`} />
+              <span className={`${styles.toggle_menu} d-flex`} />
+            </div>
+          )}
+        </div>
         {popup ? <div className={styles.popup} /> : ""}
       </header>
     </>
