6c83da53356add8ac244da3e0981c70659e6b371
Spyro7883 <spinu11_2003@yahoo.ro>
2023-09-25 09:26:54 +0300

    queries for business model component added

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,12 +1,14 @@
 import styles from "../styles/testimonials.module.scss";
 import Image from "next/image";
 import type { TestimonialPost } from "../utils";
+import useWindowWidth from "../hooks/useWindow";
 
 type TestimonialsProps = {
     testimonials: TestimonialPost[];
 }
 
 const Testimonials: React.FC<TestimonialsProps> = ({ testimonials }) => {
+    const usersWidth = useWindowWidth() || 0;
     return (
         <>
             <section
@@ -16,42 +18,46 @@ const Testimonials: React.FC<TestimonialsProps> = ({ testimonials }) => {
                 <h4 className={styles.title}>
                     Testimonials from brands you may know
                 </h4>
-                {testimonials.map((post, id) => (
-                    <div className={styles.content} key={id}>
-                        <Image
-                            src="/Hyros_Quotes.svg"
-                            alt="Quotes"
-                            width={26}
-                            height={24}
-                            priority
-                        />
-                        <p className={styles.message}>
-                            {post.author_message.map((message, message_id) => {
-                                if (typeof message === 'string') {
-                                    return <span key={message_id}>{message}</span>;
-                                }
-                                return <span key={message_id} className={styles[message.className]}>{message.content}</span>;
-                            })}
-                        </p>
-                        <div className={styles.writer}>
-                            <Image
-                                src={`/${post.author_profile}`}
-                                alt={post.author_name}
-                                width={57}
-                                height={57}
-                                priority
-                            />
-                            <div className={styles.wrapper}>
-                                <p className={styles.author}>
-                                    {post.author_name}
-                                </p>
-                                <p className={styles.description}>
-                                    {post.author_description}
-                                </p>
+                <div className={styles.group}>
+                    {testimonials.map((post, id) => (
+                        <div className={styles.content} key={id}>
+                            {usersWidth >= 287 ?
+                                <Image
+                                    src="/Hyros_Quotes.svg"
+                                    alt="Quotes"
+                                    width={26}
+                                    height={24}
+                                    priority
+                                /> : ""}
+                            <p className={styles.message}>
+                                {post.author_message.map((message, message_id) => {
+                                    if (typeof message === 'string') {
+                                        return <span key={message_id}>{message}</span>;
+                                    }
+                                    return <span key={message_id} className={styles[message.className]}>{message.content}</span>;
+                                })}
+                            </p>
+                            <div className={styles.writer}>
+                                {usersWidth >= 287 ?
+                                    <Image
+                                        src={`/${post.author_profile}`}
+                                        alt={post.author_name}
+                                        width={57}
+                                        height={57}
+                                        priority
+                                    /> : ""}
+                                <div className={styles.wrapper}>
+                                    <p className={styles.author}>
+                                        {post.author_name}
+                                    </p>
+                                    <p className={styles.description}>
+                                        {post.author_description}
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </section>
         </>)
 }
